4b872aa41a37287bfacb28a46ab393c7e846e8af
Sergey Malakhov <Kronkadet2014@icloud.com>
2020-05-04 15:26:31 +0300

    Initial commit

---
+++ PhotoTapps/Controllers/PhotoCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Sergey Malakhov on 04.05.2020.
+//  Copyright © 2020 Sergey Malakhov. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+    
+}
